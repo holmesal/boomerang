aee6c8afe4e88c89758650a4f70e13820165c2d1
holmesal <siralonsoholmes@gmail.com>
2014-05-07 14:47:17 -0700

    not using svgmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -337,7 +337,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
-            'images/{,*/}*.{webp}',
+            'images/{,*/}*.{webp,svg}',
             'fonts/*'
           ]
         }, {
@@ -368,8 +368,8 @@ module.exports = function (grunt) {
       dist: [
         'coffee',
         'compass:dist',
-        'imagemin',
-        'svgmin'
+        'imagemin'
+        // 'svgmin'
       ]
     },
 
